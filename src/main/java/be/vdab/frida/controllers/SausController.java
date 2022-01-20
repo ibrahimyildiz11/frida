db347ecb70ee6721dac6a3805d54d19793b3ce3d
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-01-20 16:34:00 +0100

    alfabet

--- src/main/java/be/vdab/frida/controllers/SausController.java
+++ src/main/java/be/vdab/frida/controllers/SausController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("sauzen")
@@ -28,5 +29,22 @@ public class SausController {
         Arrays.stream(sauzen).filter(saus -> saus.getId() == id).findFirst()
                 .ifPresent(saus -> modelAndView.addObject(saus));
         return modelAndView;
+    }
+
+    private final char[] alfabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    @GetMapping("alfabet")
+    public ModelAndView alfabet() {
+        return new ModelAndView("sausAlfabet", "alfabet", alfabet);
+    }
+
+    private Stream<Saus> sauzenDieBeginnenMet(char letter) {
+        return Arrays.stream(sauzen)
+                .filter(saus -> saus.getNaam().charAt(0) == letter);
+    }
+
+    @GetMapping("alfabet/{letter}")
+    public ModelAndView sauzenBeginnendMet(@PathVariable char letter) {
+        return new ModelAndView("sausAlfabet", "alfabet", alfabet)
+                .addObject("sauzen", sauzenDieBeginnenMet(letter).iterator());
     }
 }
